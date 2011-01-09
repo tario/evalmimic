9386018d2fa7a1f5038878ebf6fb01ccc73dd98e
Dario <darios3@gmail.com>
2011-01-09 13:44:47 -0300

    fixed spec

--- spec/evalmimic_spec.rb
+++ spec/evalmimic_spec.rb
@@ -38,17 +38,20 @@ describe Evalmimic, "evalmimic" do
 
   end
 
+   class N
+    def foo(x)
+      @a = 5
+      args = x.alternate_eval "test"
+      args[0].eval("@a")
+    end
+  end
+
 
   it "should recall eval with default arguments and eval local variables" do
     X3.define_eval_method :alternate_eval
 
     x = X3.new
-
-    args = x.alternate_eval "test"
-    args[1].should be == ["test"]
-
-    @a = 4
-    args[0].eval("@a").should be == 4
+    N.new.foo(x).should be == 5
 
   end
 
